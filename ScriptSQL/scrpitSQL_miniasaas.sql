1500edd75f4cc6b1eaeced159f75343aa0f0b0e9
Guilherme006 <137656860+Guilherme006@users.noreply.github.com>
2025-05-21 19:27:00 -0300

    Update scrpitSQL_miniasaas.sql
    Foi atualizado o script da questão 3 para agrupar pelo id e o script da questão 4 para pegar a primeira parte splitada por espaço.

--- ScriptSQL/scrpitSQL_miniasaas.sql
+++ ScriptSQL/scrpitSQL_miniasaas.sql
@@ -14,20 +14,22 @@ SELECT ca.name AS customer_name, SUM(p.value) AS total_received
 FROM customer_account AS ca
 JOIN payment AS p ON ca.id = p.customer_account_id
 WHERE p.status = 'RECEIVED'
-GROUP BY ca.name
+GROUP BY ca.id
 HAVING SUM(p.value) > 1000
 ORDER BY total_received DESC 
 LIMIT 20;
 
 -- 4. Primeiro nome dos clientes (customer) com análise geral aprovada (customer_regiter_status), ordenado por data de aprovação
-SELECT REGEXP_SUBSTR(c.name, '[A-Za-zÀ-ÿ]+') AS first_name, cs.last_general_status_change AS approval_date
+SELECT
+	SUBSTRING_INDEX(c.name, ' ', 1) AS first_name,
+  cs.last_general_status_change AS approval_date
 FROM customer AS c
 JOIN customer_register_status AS cs ON c.id = cs.customer_id
-WHERE 
-  cs.general_approval = 'APPROVED'
-  AND c.name IS NOT NULL
-  AND TRIM(c.name) <> ''
-ORDER BY cs.last_general_status_change DESC;
+WHERE cs.general_approval = 'APPROVED'
+    AND c.name IS NOT NULL
+    AND TRIM(c.name) <> ''
+ORDER BY
+	cs.last_general_status_change DESC
 
 
 
